12560ec264c9b493780724977eb220abdf76908c
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-03 15:19:45 -0600

    Favicon error - diagnosing if it's the only one

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function handlePassword(input_field) {
     password_value = document.getElementById(input_field).value;
-    
+    console.log("connected")
     fetch("secret.txt")
         .then(response => response.text())
         .then(text => console.log(text));
