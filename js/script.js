c4014684664c76be9ae1c3050d347585041cfef2
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-03 15:47:25 -0600

    Trying button instead of input submission

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function handlePassword(input_field) {
     password_value = document.getElementById(input_field).value;
     console.log("connected")
-    fetch("secret.txt", {mode: 'cors'})
+    fetch("secret.txt")
         .then(response => response.text())
         .then(text => console.log(text));
     
