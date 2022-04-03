a2ee87316b09b11310584eb2193d1681dfa32906
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-03 15:41:35 -0600

    Trying to perform fetch via CORS. Not sure if server will accept it...

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function handlePassword(input_field) {
     password_value = document.getElementById(input_field).value;
     console.log("connected")
-    fetch("secret.txt")
+    fetch("secret.txt", {mode: 'cors'})
         .then(response => response.text())
-        .catch()
-        .then(text => console.log(text))
-        .catch();
+        .then(text => console.log(text));
     
 }
