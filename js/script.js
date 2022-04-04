f2525ad2a7cd8ce6a3584f48c61cd7e54345c0b3
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-04 11:42:21 -0600

    Including password check inside fetch call

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 function handlePassword(input_field) {
     password_value = document.getElementById(input_field).value;
-    let retrieved_password = "";
     fetch("secret.txt")
         .then(response => response.text())
-        .then(text => {retrieved_password = text});
-    console.log("Retrieved:", retrieved_password);
-    console.log("Inputted:", password_value);
-    if (password_value == retrieved_password) {
+        .then(text => comparePasswords(password_value, text));
+    
+}
+
+function comparePasswords(input, retrieved) {
+    console.log("Retrieved:", input);
+    console.log("Inputted:", retrieved);
+    if (input == retrieved) {
         console.log("Correct password");
     }
     else {
