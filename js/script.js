47a135a4df38b6ec38cae7db7b1aa405ac2ebdbe
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-04 11:17:47 -0600

    Added code to check for correct password for lock1 (may become lock2?)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function handlePassword(input_field) {
     password_value = document.getElementById(input_field).value;
-    console.log("connected")
+    let retrieved_password = "";
     fetch("secret.txt")
         .then(response => response.text())
-        .then(text => console.log(text));
-    
+        .then(text => retrieved_password = text);
+
+    if (password_value == retrieved_password) {
+        console.log("Correct password");
+    }
+    else {
+        console.log("Incorrect password");
+    }
 }
