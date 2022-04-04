18fc36d6fc2481f244d640b56a68b697750629ca
Jaren Campbell <jarenrcampbell@gmail.com>
2022-04-04 12:24:33 -0600

    Moved lock 1 to lock 2, implemented new lock 1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-function handlePassword(input_field) {
+function handleButton1(input_field) {
+    password_value = document.getElementById(input_field).value;
+    if (password_value == "iamthesenate") {
+        console.log("Correct password for lock 1");
+    }
+    else {
+        console.log("Incorrect password for lock 1");
+    }
+}
+
+function handleButton2(input_field) {
     password_value = document.getElementById(input_field).value;
     fetch("secret.txt")
         .then(response => response.text())
         .then(text => comparePasswords(password_value, text));
-    
 }
 
 function comparePasswords(input, retrieved) {
     console.log("Retrieved:", input);
     console.log("Inputted:", retrieved);
     if (input == retrieved) {
-        console.log("Correct password");
+        console.log("Correct password for lock 2");
     }
     else {
-        console.log("Incorrect password");
+        console.log("Incorrect password for lock 2");
     }
 }
